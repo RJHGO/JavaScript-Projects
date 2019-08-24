6ab0018cd2e6bbb1146099221336250cf8d6f104
RJHGO <53460219+RJHGO@users.noreply.github.com>
2019-08-24 12:57:15 -0700

    Add files via upload
    Updated JS and CSS.

--- Basic JavaScript Projects/JS/Basic_JavaScript_9.js
+++ Basic JavaScript Projects/JS/Basic_JavaScript_9.js
@@ -11,3 +11,32 @@ function countdown(){
     } 
     tick();
 }
+
+var slideIndex = 1;
+showSlides(slidesIndex);
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n){
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n){
+    var x;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for(x = 0; x < slides.length; x++){
+        slides[x].style.display = "none";
+    }
+
+    for (x = 0; x < dots.length; x++) {
+        dots[x].className = dots[x].className.replace("active", "");
+
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
